b049c11bb16db44e1b14e11cc3e08dd74112a5e1
Arthur <artur.dzhenbaz@gmail.com>
2024-06-01 16:55:31 +0300

    Commit 4

--- app/src/main/java/com/example/musicplayercoursework/AboutAuthorFragment.java
+++ app/src/main/java/com/example/musicplayercoursework/AboutAuthorFragment.java
@@ -14,7 +14,6 @@ public class AboutAuthorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_author, container, false);
     }
 }
--- app/src/main/java/com/example/musicplayercoursework/AboutProgramFragment.java
+++ app/src/main/java/com/example/musicplayercoursework/AboutProgramFragment.java
@@ -14,7 +14,6 @@ public class AboutProgramFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_program, container, false);
     }
 }
